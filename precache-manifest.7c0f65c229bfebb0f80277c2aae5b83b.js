a9e2de002d50b8730bb99dca65f4ea3312f98451
vero <vero.urrutiaa@gmail.com>
2020-05-04 09:08:06 -0400

    Updates

---
+++ precache-manifest.7c0f65c229bfebb0f80277c2aae5b83b.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3fcbb70cf1a3bf174dcdefd9e40a920",
+    "url": "/Portafolio/index.html"
+  },
+  {
+    "revision": "35932c9f4cf6cd86e352",
+    "url": "/Portafolio/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "684ac0aa7a9cec45d581",
+    "url": "/Portafolio/static/css/main.f8c509a2.chunk.css"
+  },
+  {
+    "revision": "35932c9f4cf6cd86e352",
+    "url": "/Portafolio/static/js/2.d933dfce.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Portafolio/static/js/2.d933dfce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "684ac0aa7a9cec45d581",
+    "url": "/Portafolio/static/js/main.850bf6e6.chunk.js"
+  },
+  {
+    "revision": "fba241b40bd15cfd5948",
+    "url": "/Portafolio/static/js/runtime-main.0d8c31b5.js"
+  },
+  {
+    "revision": "ffded2381d25dd906406d6e07f91dea5",
+    "url": "/Portafolio/static/media/bootstrap-4.ffded238.svg"
+  },
+  {
+    "revision": "3707da3c6a394c7ee2523273f8d98a25",
+    "url": "/Portafolio/static/media/css3.3707da3c.svg"
+  },
+  {
+    "revision": "7f8d489c1887554fcf61932505d46393",
+    "url": "/Portafolio/static/media/firebase-1.7f8d489c.svg"
+  },
+  {
+    "revision": "777a3d3f5869bbf12deb07761831886a",
+    "url": "/Portafolio/static/media/fondo.777a3d3f.jpg"
+  },
+  {
+    "revision": "1190a19ce3cc940741a60137a1e074cb",
+    "url": "/Portafolio/static/media/git-icon.1190a19c.svg"
+  },
+  {
+    "revision": "0ff1a6f08182be4aa63a0b54da1b9e57",
+    "url": "/Portafolio/static/media/github-1.0ff1a6f0.svg"
+  },
+  {
+    "revision": "e67c6af8237338b75bc64b81a1c7d096",
+    "url": "/Portafolio/static/media/gmail-icon.e67c6af8.svg"
+  },
+  {
+    "revision": "676e8188f003ed5d4882a0d0f378dad3",
+    "url": "/Portafolio/static/media/html-5.676e8188.svg"
+  },
+  {
+    "revision": "61ed8b054178d0e8d1285ad8f29f4045",
+    "url": "/Portafolio/static/media/javascript.61ed8b05.svg"
+  },
+  {
+    "revision": "4f2bc978122ea69dc7033f2f60cb0fe5",
+    "url": "/Portafolio/static/media/linkedin-icon-2.4f2bc978.svg"
+  },
+  {
+    "revision": "4009eef3b3e291f296e71962c70895c5",
+    "url": "/Portafolio/static/media/react.4009eef3.svg"
+  },
+  {
+    "revision": "3fb894d8d260e0c8a3b1beb10bab3bd2",
+    "url": "/Portafolio/static/media/v.3fb894d8.jpg"
+  },
+  {
+    "revision": "b15a4ed22cbbd202c466d5bfea35b49d",
+    "url": "/Portafolio/static/media/yo.b15a4ed2.jpeg"
+  }
+]);
